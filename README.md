903f686ac0c3c141faa8ec4bc15bed0147683ddb
Lucas Bassetti <lucasbr.dafonseca@gmail.com>
2019-12-03 19:17:53 -0300

    Add new example models

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # OntoUML Example Models
 
 Repository with OntoUML models for development tests
+
+## Invalid OntoUML Models
+
+| Model | Valid Model | Valid OntoUML Model | Error Description |
+|:----:|:----:|:----:| :----:| 
+| m1.invalid.json | ✅ | ❌| Stereotypes missing |
+| m2.invalid.json | ✅ | ❌| Stereotypes missing |
+| m3.invalid.json | ✅ | ❌| Invalid Stereotype |
+| m4.invalid.json | ✅ | ❌| Endurant sortal `role` without specialization |
+| m5.invalid.json | ✅ | ❌| Kind cannot specialize another kind |
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 module.exports = {
-  modelExample1: require('./models/m1.example.json'),
-  modelExample2: require('./models/m2.example.json')
+  // Invalid models
+  modelInvalidExample1: require('./models/invalids/m1.invalid.json'),
+  modelInvalidExample2: require('./models/invalids/m2.invalid.json'),
+  modelInvalidExample3: require('./models/invalids/m3.invalid.json'),
+  modelInvalidExample4: require('./models/invalids/m4.invalid.json'),
+  modelInvalidExample5: require('./models/invalids/m5.invalid.json'),
+  // Valid models
+  modelValidExample1: require('./models/valids/m1.valid.json'),
 }
